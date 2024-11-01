f88b7da763ba8f84d6fd91f353f9845e7fe22102
joosep <joosep.kurg@gmail.com>
2024-11-01 09:36:27 +0200

    [200~Ülesanne: Funktsiooni mõiste

--- funktsiooni_m6iste.py
+++ funktsiooni_m6iste.py
@@ -1,34 +1,55 @@
 """Basic function exercises."""
 import math
 
+
 def print_hello():
-    """Prints "Hello"."""
+    """Print "hello"."""
+    # code here
     print("Hello")
 
+
 def get_hello() -> str:
-    """Returns "Hello"."""
+    """Return "hello"."""
+    # code here
     return "Hello"
 
+
 def ask_name_and_greet_user():
-    """Asks for user's name and greets."""
+    """
+    Ask name and greet user.
+
+    The user has to enter his/her name. The function prints the greeting.
+
+    Regular greeting: Hi, [name]. Would you like to have a Hamburger?
+    [name] is capitalized, meaning first letter is capital, the rest is lower.
+
+    If the name is Thanos (case insensitive, so thanos and THANOS also count):
+    Get out of here, Thanos! Nobody wants to play with you!
+    """
+    # code here
     name = input("What's your name? ")
     if name.lower() == 'thanos':
         print("Get out of here, Thanos! Nobody wants to play with you!")
     else:
         print(f"Hi, {name.capitalize()}. Would you like to have a Hamburger?")
 
+
 def calculate_hypotenuse_length(a: float, b: float) -> float:
-    """Calculates and returns hypotenuse length."""
+    """Return hypotenuse value."""
+    # code here
     return math.sqrt(a ** 2 + b ** 2)
 
+
 def calculate_cathetus_length(a: float, c: float) -> float:
-    """Calculates and returns cathetus length."""
+    """Return cathetus value."""
+    # code here
     return math.sqrt(c ** 2 - a ** 2)
 
+
 if __name__ == '__main__':
-    print_hello()  
-    hello = get_hello()  
-    print(hello)  
-    ask_name_and_greet_user()  
-    print(calculate_hypotenuse_length(3, 4))  
-    print(calculate_cathetus_length(3, 5))  
+    print_hello()  # should print "Hello"
+    hello = get_hello()  # should return "Hello"
+    print(hello)  # let's check the value of hello variable
+    ask_name_and_greet_user()  # should ask name and greet
+    print(calculate_hypotenuse_length(3, 4))  # should print 5.0
+    print(calculate_cathetus_length(3, 5))  # should print 4.0
