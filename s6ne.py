63ccdec69f00458bcb0e8ba8e06958fc14717c2b
joosep <joosep.kurg@gmail.com>
2024-11-01 09:07:33 +0200

    Sõne

---
+++ s6ne.py
@@ -0,0 +1,20 @@
+# Write your code here
+
+#Ülesanne 1
+first_name = "James"
+last_name = "Bond"
+
+#Ülesanne 2
+full_name = first_name + " " + last_name
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+
+#Ülesanne 3
+cake = "vahukoor\nmarjad\ntäidis\npõhi"
+print(cake)  
+
+#Ülesanne 4
+original_string = "Programming is fun!"
+
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[:11][::-1]
